db584eb5e127f76297f8b2700fac38fb5e6e8bc0
madhan <120784875+madhanmaaz@users.noreply.github.com>
2023-10-07 19:36:51 +0530

    update token-auth

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const http = require("http")
 const cookieParser = require("cookie-parser")
 const fileUpload = require("express-fileupload")
+const config = require("./ssd/config.json")
 
 const app = express()
 const server = http.createServer(app)
@@ -24,14 +25,21 @@ app.use(function (req, res, next) {
 
 app.use("/", require("./route/"))
 app.use("/mimic", require("./route/mimic"))
+app.use(function tokenCheck(req, res, next) {
+    const token = req.cookies.token
+    if (token != undefined && token == config.token) {
+        next()
+    } else {
+        res.redirect("/")
+    }
+})
 app.use("/panel", require("./route/panel"))
-
 
 
 const PORT = 3410 || process.env.PORT
 server.listen(PORT, () => {
     require("./utils/banner")
-    console.log(`PORT: ${PORT}
+    console.log(`URL: http://localhost:${PORT}/
 USERNAME: admin
 PASSWORD: admin
 > want to change ./ssd/config.json
